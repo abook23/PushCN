e4b3ef892b158ae5e6612209a6f1cfb6aff2dd13
yangxiong <abook23@163.com>
2018-12-18 15:12:02 +0800

    1111

---
+++ push-xg/src/main/java/com/pus/xg/XGBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.pus.xg;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by My on 2018/2/27.
+ */
+
+public class XGBroadcastReceiver extends  {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
